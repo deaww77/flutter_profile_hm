5d0001d03e44303a01d9e20775a25c486834370a
deaww77 <126137481+deaww77@users.noreply.github.com>
2025-06-27 14:55:09 +0700

    add lyout and text

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,97 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title:Text("My Flutter app"),
+          title: Text("My Flutter app"),
           backgroundColor: const Color.fromARGB(255, 186, 224, 255),
-          ),
+        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.asset("assets/images/som.jpg")
+            Image.asset("assets/images/som.jpg"),
+            Text(
+              "Student Chanon Roobkhum",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: const Text(
+                "นักศึกษาสาขาวิทยาการคอมพิวเตอร์, \nคณะศิลปศาสตร์และวิทยาศาตร์ \nมหาวิทยาลัยราชภัฎศรีสะเกษ",
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: const Color.fromARGB(255, 235, 255, 18),
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: const Color.fromARGB(255, 235, 255, 18),
+                      ),
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                    ],
+                  ),
+
+                  Text(
+                    "170 Reviews",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: List.generate(50, (index) {
+                return Expanded(
+                  child: Container(
+                    color: index.isEven ? Colors.grey : Colors.transparent,
+                    height: 1,
+                  ),
+                );
+              }),
+            ),
+
+            const SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.phone, color: Colors.black),
+                      Text("Phone", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.email, color: Colors.black),
+                      Text("Email", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.facebook, color: Colors.black),
+                      Text("Facebook", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
